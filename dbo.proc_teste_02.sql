1c04052e2136f66b377e3d0b1db8c88fa3b5f1e2
João Henrique Moreira Fabel <joaofabel@gmail.com>
2023-04-21 23:58:43 -0300

    x x x

--- dbo.proc_teste_02.sql
+++ dbo.proc_teste_02.sql
@@ -8,7 +8,7 @@ CREATE PROCEDURE [dbo].[proc_teste_02] (
 AS
 BEGIN
     DECLARE @Nome varchar(51)
-           ,@Endereco varchar(777)
+           ,@Endereco varchar(888)
            .@CEP varchar(10)
 
     IF EXISTS(
